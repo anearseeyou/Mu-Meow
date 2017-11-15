371373538a40c8e3856e3aa2be0a8580b980d982
Barry <369916324@qq.com>
2017-11-15 14:24:04 +0800

    commit mine done

--- src/components/mine/mine.vue
+++ src/components/mine/mine.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="mine-wrap">
-        <div class="mine-bgc">
+        <div class="mine-head">
             <!-- 用户头像 -->
             <div class="mine-info">
                 <img class="avart" :src="avatar" @click="avatarUpload">
@@ -9,38 +9,38 @@
             </div>
         </div>
         <!-- 用户信息 -->
-        <div class="mine-info">
+        <div class="mine-operation">
             <a class="user-list" @click="myCollect">
-                <span class="sc-bar block fl"></span>
-                <span class="sc-text block fl">我收藏的</span>
-                <span class="sc-open block fr"></span>
+                <span class="mine-bar mine-collect"></span>
+                <span class="mine-text">我收藏的</span>
+                <span class="mine-open"></span>
             </a>
             <a class="user-list" @click="myLiked">
-                <span class="sc-bar block mine-fadu fl"></span>
-                <span class="sc-text block fl">我赞过的</span>
-                <span class="sc-open block fr"></span>
+                <span class="mine-bar mine-like"></span>
+                <span class="mine-text">我赞过的</span>
+                <span class="mine-open"></span>
             </a>
             <a class="user-list" @click="myComment">
-                <span class="sc-bar block mine-comment  fl"></span>
-                <span class="sc-text block fl ">我评论的</span>
-                <span class="sc-open block fr"></span>
+                <span class="mine-bar mine-comment"></span>
+                <span class="mine-text">我评论的</span>
+                <span class="mine-open"></span>
             </a>
             <a class="user-list" @click="myNews">
-                <span class="sc-bar block mine-news fl">
+                <span class="mine-bar block mine-news">
                     <i v-show="noneNews" class="remind-news"></i>
                 </span>
-                <span class="sc-text block fl">我的消息</span>
-                <span class="sc-open block fr"></span>
+                <span class="mine-text">我的消息</span>
+                <span class="mine-open"></span>
             </a>
             <a class="user-list" @click="myAbout">
-                <span class="sc-bar block mine-about fl"></span>
-                <span class="sc-text block fl">关于我们</span>
-                <span class="sc-open block fr"></span>
+                <span class="mine-bar mine-about"></span>
+                <span class="mine-text">关于我们</span>
+                <span class="mine-open"></span>
             </a>
             <a class="user-list" @click="signOut">
-                <span class="sc-bar block mine-signout fl"></span>
-                <span class="sc-text block fl">退出登录</span>
-                <span class="sc-open block fr"></span>
+                <span class="mine-bar mine-signout"></span>
+                <span class="mine-text">退出登录</span>
+                <span class="mine-open"></span>
             </a>
         </div>
     </div>
@@ -93,93 +93,89 @@
 
 <style lang="less" rel="stylesheet/less">
 
-    .mine-wrap{
-        .mine-bgc {
+    .mine-wrap {
+        .mine-head {
             width: 100%;
             height: 410px;
-            overflow: hidden;
+            margin-bottom: 40px;
             background-size: 100% 100%;
             background: url("img/mine.png") no-repeat;
             .mine-info {
-                width: 210px;
+                width: 200px;
                 position: absolute;
                 left: 50%;
                 top: 165px;
-                margin-left: -105px;
+                margin-left: -100px;
                 font-size: 30px;
                 text-align: center;
                 .avart {
-                    width: 210px;
-                    width: 200px;
+                    width: 180px;
+                    width: 180px;
                     border-radius: 50%;
                     margin-bottom: 10px;
                 }
             }
         }
-    }
-
-    .mine-info {
-        width: 100%;
-        .user-list {
-            display: block;
-            height: 100px;
-            line-height: 100px;
-            padding:0 30px;
-            border-bottom: 1px solid #ededed;
-            .sc-bar, .sc-open {
-                width: 90px;
-                height: 100%;
-                background: url("img/mine-recom.png") no-repeat center center;
-                background-size: 30px 30px;
-            }
-            .sc-open {
-                margin-right: 58px;
-                background: url("img/info-gt.png") no-repeat center center;
-                background-size: 22px 44px;
-            }
-            .sc-text {
-                color: #161619;
-                font-size: 26px;
-                margin-left: 30px;
-            }
-            .mine-fadu {
-                background: url("img/mine-like.png") no-repeat center center;
-                background-size: 50px 50px;
-            }
-            .mine-comment {
-                background: url("img/mine-write.png") no-repeat center center;
-                background-size: 50px 50px;
-            }
-            .mine-news {
-                background: url("img/mine-news.png") no-repeat center center;
-                background-size: 2.5rem 2.5rem;
-                position: relative;
-                .remind-news {
-                    content: '';
-                    display: block;
-                    width: 20px;
-                    height: 20px;
-                    border-radius: 50%;
-                    background-color: #fb2d2d;
-                    position: absolute;
-                    right: 10px;
-                    top: 30px;
+        .mine-operation {
+            width: 100%;
+            .user-list {
+                display: flex;
+                height: 100px;
+                line-height: 100px;
+                padding: 0 30px;
+                border-bottom: 1px solid #ededed;
+                .mine-bar, .mine-open {
+                    flex: 1;
                 }
-            }
-            .mine-about {
-                background: url("img/mine-about.png") no-repeat center center;
-                background-size: 2.5rem 2.5rem;
-            }
-            .mine-signout {
-
-                margin-bottom: 200px;
-                background: url("img/mine-signout.png") no-repeat center center;
-                background-size: 2.5rem 2.5rem;
+                .mine-open {
+                    background: url("img/info-gt.png") no-repeat right;
+                    background-size: 16px 30px;
+                }
+                .mine-text {
+                    flex: 8;
+                    color: #161619;
+                    font-size: 26px;
+                    margin-left: 10px;
+                }
+                .mine-collect {
+                    background: url("img/mine-recom.png") no-repeat center center;
+                    background-size: 30px 30px;
+                }
+                .mine-like {
+                    background: url("img/mine-like.png") no-repeat center center;
+                    background-size: 30px 30px;
+                }
+                .mine-comment {
+                    background: url("img/mine-write.png") no-repeat center center;
+                    background-size: 30px 30px;
+                }
+                .mine-news {
+                    background: url("img/mine-news.png") no-repeat center center;
+                    background-size: 30px 30px;
+                    position: relative;
+                    .remind-news {
+                        content: '';
+                        display: block;
+                        width: 20px;
+                        height: 20px;
+                        border-radius: 50%;
+                        background-color: #fb2d2d;
+                        position: absolute;
+                        right: 10px;
+                        top: 30px;
+                    }
+                }
+                .mine-about {
+                    background: url("img/mine-about.png") no-repeat center center;
+                    background-size: 30px 30px;
+                }
+                .mine-signout {
+                    background: url("img/mine-signout.png") no-repeat center center;
+                    background-size: 30px 30px;
+                }
             }
         }
     }
 
-    .news-wrap {
-        margin-top: 132px;
-    }
+
 </style>
